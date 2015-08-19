a04038e11c7aa007927be31d9a02249411ac3f50
heliogabal <rhalbmann@gmail.com>
2015-08-19 11:16:06 +0200

    add datenschutzerklaerung fix mobile menu

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -56,6 +56,21 @@
   }
 ?>
 <?php print $page_bottom; //stuff from modules always render last ?>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//inglist.de/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 40]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//inglist.de/piwik/piwik.php?idsite=40" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 </body>
 </html>
 
